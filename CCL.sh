806a669f28734d669a6dc922d9385792150c4d3b
hummerstudio <hummerstudio@163.com>
2016-05-26 18:30:53 +0800

    Create CCL.sh
    首次更新

---
+++ CCL.sh
@@ -0,0 +1,28 @@
+alias 别名="alias"
+
+#基本命令
+alias 进="cd"
+alias 退=".."
+alias 回家="cd ~"
+alias 列="ls -l"
+
+alias 删除文件="rm"
+alias 删除文件夹="rm -r"
+
+alias 历史="history"
+
+alias 文件夹大小="du -sh"
+
+alias 分区使用情况="df -Th"
+
+#软件管理
+alias 下载="sudo apt-get download"
+alias 安装="sudo apt-get install"
+alias 卸载="sudo apt-get --purge remove"
+
+alias 升级软件="sudo apt-get update"
+#alias 升级系统=""
+
+alias 解压tar.gz="tar -zxvf"
+alias 解压tar="tar -xvf"
+alias 解压zip="unzip"
